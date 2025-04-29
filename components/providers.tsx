55041d4c3d9a03be182a55c0fb4251bb72b74787
fixonz <fixzzee@gmail.com>
2025-04-29 22:17:39 +0300

    fix:

--- components/providers.tsx
+++ components/providers.tsx
@@ -11,7 +11,7 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <AbstractWalletProvider chain={abstractTestnet}>
+    <AbstractWalletProvider config={abstractTestnet}>
       <WalletProvider>{children}</WalletProvider>
     </AbstractWalletProvider>
   )
