9f51b73250ac8246cdde09938f0ea584776fa737
fixonz <fixzzee@gmail.com>
2025-04-30 12:16:38 +0300

    fix: final

--- components/root-providers.tsx
+++ components/root-providers.tsx
@@ -2,10 +2,13 @@
 
 import type { ReactNode } from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
-// Create a client
-const queryClient = new QueryClient()
+import { useRef } from "react"
 
 export default function RootProviders({ children }: { children: ReactNode }) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+  if (!queryClientRef.current) return null;
+  return <QueryClientProvider client={queryClientRef.current}>{children}</QueryClientProvider>;
 }
